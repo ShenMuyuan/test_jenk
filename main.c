fca3caa4eb0472b10db21d2fd31bf80de9cef041
ShenMuyuan <u202013816@hust.edu.cn>
2022-10-16 17:22:51 +0800

    add input and output function for initial testing

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUF_SIZE 10
 
 int main()
 {
-  printf("Hello\n");
-  return 0;
+    FILE *in, *out;
+    in = freopen("input.txt", "r", stdin);
+    char s[10];
+    scanf("%s", s);
+    int n;
+    scanf("%d", &n);
+    fclose(in);
+    fprintf(stdout, "player %d\n", n);
+    out = freopen("output.txt", "w", stdout);
+    fprintf(stdout, "player %d\n", n);
+    fclose(out);
+    return 0;
 }
--- test.sh
+++ test.sh
@@ -1,2 +1,8 @@
+num=$1
+echo "step $num" > input.txt
+
+make clean
+make
+
 ./test_jenk_proj > test_out
 diff output.txt test_out
